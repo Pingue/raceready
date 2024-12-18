51082d10178f4ccf331b0a8a34f8123a77e23b32
Mike Frost <me@mfrost.co.uk>
2024-12-18 18:10:37 +0000

    fix: Add default host and port when running

--- raceready.py
+++ raceready.py
@@ -189,6 +189,5 @@ def handle_reset_all():
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
-    #app.run(debug=True, host='0.0.0.0', port=port)
     print("app running")
-    socketio.run(app)
+    socketio.run(app, host='0.0.0.0', port=port)
